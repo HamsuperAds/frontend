21a07e53884ecf6b3a0edfe3e23518853050fc34
Chizoba <upc4you@gmail.com>
2026-02-13 14:17:36 +0100

    fix: fixed user profile edit to allow edit of unverified phone number

--- app/pages/account/index.vue
+++ app/pages/account/index.vue
@@ -59,7 +59,8 @@
                 <!-- Phone Number -->
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Phone number</label>
-                    <input v-model="formData.phone_number" readonly type="tel"
+                    <input v-model="formData.phone_number" :readonly="$getUser()!.phone_number_verified_at != null"
+                        type="tel"
                         class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         placeholder="08023234345" />
                 </div>
